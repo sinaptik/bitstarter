c43a6a90952c7064df486b6aab1b70c64bc350fd
Sergei <sinaptik@what9000>
2013-07-10 18:44:49 +1200

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
-var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) 
 {
+  var fs = require('fs');
+
   var index = fs.readFile('./index.html', 'utf8');
 
   response.send(index);
